ba3ec6b16c3c782f2f14168f105de2b475b668cb
defnealtiok123 <defnealtiok1983@gmail.com>
2023-11-07 01:53:30 -0500

    Create SignupViewModel

--- src/interface_adapter/signup/SignupViewModel.java
+++ src/interface_adapter/signup/SignupViewModel.java
@@ -1,4 +1,14 @@
-package interface_adapter.signup;
+package src.interface_adapter.signup;
+
+import java.beans.PropertyChangeListener;
 
 public class SignupViewModel {
+    private String viewName;
+
+    public SignupViewModel(String viewName) {
+        this.viewName = viewName;
+    }
+    public String getViewName() {
+        return this.viewName;
+    }
 }
