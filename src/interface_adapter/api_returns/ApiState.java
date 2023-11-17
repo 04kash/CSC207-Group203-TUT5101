01511cc719058fd07dc793bc66a62786f2c25dbf
04kash <m04kashish@gmail.com>
2023-11-16 19:44:22 -0500

    Resolved an issue with ApiPresenter

--- src/interface_adapter/api_returns/ApiState.java
+++ src/interface_adapter/api_returns/ApiState.java
@@ -10,6 +10,7 @@ public class ApiState {
     private String filter = "";
     private Coordinate proximityLocation = new Coordinate(0.0, 0.0);
     private double proximityDistance = 0.0;
+    private String locationError;
 
     public ApiState(ApiState copy) {
         username = copy.username;
@@ -68,5 +69,9 @@ public class ApiState {
 
     public void setProximityDistance(double proximityDistance) {
         this.proximityDistance = proximityDistance;
+    }
+
+    public void setLocationError(String error) { //Added this, Is it needed?
+        this.locationError = error;
     }
 }
