913f6fed795ad7963df62a30ea914a288d0e2009
04kash <m04kashish@gmail.com>
2023-11-23 14:20:48 -0500

    Fixed LoggedIn View and add SearchView

--- src/view/LoggedInView.java
+++ src/view/LoggedInView.java
@@ -1,69 +1,16 @@
 package view;
 
 import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JRadioButton;
-import javax.swing.JLabel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JTextField;
-import javax.swing.JList;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
-import java.awt.Color;
 
 public class LoggedInView extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private JTextField textField;
-	private JTextField textField_1;
 
 	/**
 	 * Create the panel.
 	 */
 	public LoggedInView() {
-		setBackground(new Color(204, 255, 255));
-		this.setLayout(null);
-		
-		JButton btnNewButton = new JButton("Saved Locations");
-		btnNewButton.setForeground(new Color(255, 255, 255));
-		btnNewButton.setBackground(new Color(255, 255, 255));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(289, 11, 151, 23);
-		add(btnNewButton);
-		
-		JLabel lblNewLabel = new JLabel("Location:");
-		lblNewLabel.setBounds(76, 74, 80, 14);
-		add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Filter:");
-		lblNewLabel_1.setBounds(76, 116, 48, 14);
-		add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_3 = new JLabel("Proximity:");
-		lblNewLabel_3.setBounds(76, 154, 59, 14);
-		add(lblNewLabel_3);
-		
-		textField = new JTextField();
-		textField.setBounds(154, 71, 96, 20);
-		add(textField);
-		textField.setColumns(10);
-		
-		JList list = new JList();
-		list.setBounds(199, 131, 0, 0);
-		add(list);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"banks ", "foods", "shops", "transport"}));
-		comboBox.setBounds(154, 112, 96, 22);
-		add(comboBox);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(154, 151, 96, 20);
-		add(textField_1);
-		textField_1.setColumns(10);
+
 	}
+
 }
---
+++ src/view/SearchView.java
@@ -0,0 +1,67 @@
+package view;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Color;
+
+public class SearchView extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JTextField textField;
+
+	/**
+	 * Create the panel.
+	 */
+	public SearchView() {
+		setBackground(new Color(204, 255, 255));
+		this.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Go to Homepage");
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setBackground(new Color(0, 0, 0));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(289, 11, 151, 23);
+		add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Enter the City you want to explore:");
+		lblNewLabel.setBounds(10, 114, 189, 14);
+		add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Choose a filter:");
+		lblNewLabel_1.setBounds(10, 163, 123, 14);
+		add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(229, 111, 172, 20);
+		add(textField);
+		textField.setColumns(10);
+		
+		JList list = new JList();
+		list.setBounds(199, 131, 0, 0);
+		add(list);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"interesting_places", "banks ", "foods", "shops", "transport"}));
+		comboBox.setBounds(229, 159, 172, 22);
+		add(comboBox);
+		
+		JButton btnNewButton_1 = new JButton("Enter");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(188, 227, 89, 23);
+		add(btnNewButton_1);
+	}
+}
