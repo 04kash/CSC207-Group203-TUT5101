7ce2a5671ecf4a6ba19e5d84fa07534239c343fd
akshayadeepak <akshayad0301@gmail.com>
2023-11-24 14:51:37 -0500

    Added the use case factories for login and sign up

---
+++ src/app/LoginUseCaseFactory.java
@@ -0,0 +1,56 @@
+package app;
+
+import entity.CommonUserFactory;
+import entity.UserFactory;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.api_returns.ApiViewModel;
+import interface_adapter.login.LoginController;
+import interface_adapter.login.LoginPresenter;
+import interface_adapter.login.LoginViewModel;
+import use_case.login.LoginInputBoundary;
+import use_case.login.LoginOutputBoundary;
+import use_case.login.LoginInteractor;
+import use_case.login.LoginUserDataAccessInterface;
+import view.LogInView;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class LoginUseCaseFactory {
+
+    /** Prevent instantiation. */
+    private LoginUseCaseFactory() {}
+
+    public static LogInView create(
+            ViewManagerModel viewManagerModel,
+            LoginViewModel loginViewModel,
+            ApiViewModel loggedInViewModel,
+            LoginUserDataAccessInterface userDataAccessObject) {
+
+        try {
+            LoginController loginController = createLoginUseCase(viewManagerModel, loginViewModel, loggedInViewModel, userDataAccessObject);
+            return new LogInView(loginViewModel, loginController);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Could not open user data file.");
+        }
+
+        return null;
+    }
+
+    private static LoginController createLoginUseCase(
+            ViewManagerModel viewManagerModel,
+            LoginViewModel loginViewModel,
+            ApiViewModel loggedInViewModel,
+            LoginUserDataAccessInterface userDataAccessObject) throws IOException {
+
+        // Notice how we pass this method's parameters to the Presenter.
+        LoginOutputBoundary loginOutputBoundary = new LoginPresenter(viewManagerModel, loggedInViewModel, loginViewModel);
+
+        UserFactory userFactory = new CommonUserFactory();
+
+        LoginInputBoundary loginInteractor = new LoginInteractor(
+                userDataAccessObject, loginOutputBoundary);
+
+        return new LoginController(loginInteractor);
+    }
+}
