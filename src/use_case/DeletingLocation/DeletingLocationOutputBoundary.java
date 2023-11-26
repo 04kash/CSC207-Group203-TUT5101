b2a9c615d53f87c9eca93a2e7c78493d8e1b24b1
defnealtiok123 <defnealtiok1983@gmail.com>
2023-11-26 14:16:12 -0500

    Fix DeletingLocationOutputBoundary

--- src/use_case/DeletingLocation/DeletingLocationOutputBoundary.java
+++ src/use_case/DeletingLocation/DeletingLocationOutputBoundary.java
@@ -1,5 +1,5 @@
 package use_case.DeletingLocation;
 
-public class DeletingLocationOutputBoundary {
+public interface DeletingLocationOutputBoundary {
     void prepareSuccessView(DeletingLocationOutputData location);
 }
