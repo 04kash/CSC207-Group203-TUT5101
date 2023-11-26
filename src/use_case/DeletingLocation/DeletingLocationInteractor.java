04651a21dd3d388740d8d69cc4420d766399371e
defnealtiok123 <defnealtiok1983@gmail.com>
2023-11-26 00:03:25 -0500

    Create DeletingLocationInteractor

--- src/use_case/DeletingLocation/DeletingLocationInteractor.java
+++ src/use_case/DeletingLocation/DeletingLocationInteractor.java
@@ -1,4 +1,23 @@
 package use_case.DeletingLocation;
 
-public class DeletingLocationInteractor {
+import java.util.ArrayList;
+
+public class DeletingLocationInteractor implements DeletingLocationInputBoundary {
+    final DeletingLocationUserDataAccessInterface userDataAccessObject;
+    final DeletingLocationOutputBoundary deletingLocationOutputBoundary;
+
+    public DeletingLocationInteractor(DeletingLocationUserDataAccessInterface deletingLocationDataAccessInterface, DeletingLocationOutputBoundary deletingLocationOutputBoundary) {
+        this.userDataAccessObject = deletingLocationDataAccessInterface;
+        this.deletingLocationOutputBoundary = deletingLocationOutputBoundary;
+    }
+
+
+    //@Override
+    public void execute(DeletingLocationInputData deletingLocationInputData) {
+        ArrayList<String> location = userDataAccessObject.getLocation();
+        DeletingLocationOutputData data = new DeletingLocationOutputData(location);
+        userDataAccessObject.clear();
+        deletingLocationOutputBoundary.prepareSuccessView(data);
+
+    }
 }
