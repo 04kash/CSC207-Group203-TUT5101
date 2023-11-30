c2ba5650e4ff11dd94dcd74c0a547fa926e68632
Riley Fewer <rileyfewer@Rileys-MacBook-Pro-2.local>
2023-11-30 15:09:44 -0500

    Label entity test finished and passing

---
+++ test/entity/LabelTest.java
@@ -0,0 +1,21 @@
+package entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LabelTest {
+    private Label label;
+
+    @BeforeEach
+    void init() {
+        Label label = new Label("Hotels");
+        this.label = label;
+    }
+
+    @Test
+    void getTitle() {
+        assertEquals("Hotels", label.getTitle());
+    }
+}
