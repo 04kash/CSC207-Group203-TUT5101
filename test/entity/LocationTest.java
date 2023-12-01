f53afbe9d2edcfc9f30275b4be009411609f1e93
Riley Fewer <rileyfewer@Rileys-MacBook-Pro-2.local>
2023-12-01 13:26:17 -0500

    I am gettig an issue with the LocationsFromLabelInterator, but the file is completed

---
+++ test/entity/LocationTest.java
@@ -0,0 +1,39 @@
+package entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationTest {
+    private Location location;
+    private Coordinate coordinate;
+
+    @BeforeEach
+    void init() {
+    Coordinate coordinate = new Coordinate(1.234, 5.678);
+    location = new Location("McDonalds", coordinate, "abc", "interesting places" );
+    this.location = location;
+    this.coordinate = coordinate;
+    }
+
+    @Test
+    void getFilter() {
+        assertEquals("interesting places", location.getFilter());
+    }
+
+    @Test
+    void getCoordinate() {
+        assertEquals(coordinate, location.getCoordinate());
+    }
+
+    @Test
+    void getName() {
+        assertEquals("McDonalds", location.getName());
+    }
+
+    @Test
+    void getOsmLink() {
+        assertEquals("abc", location.getOsmLink());
+    }
+}
