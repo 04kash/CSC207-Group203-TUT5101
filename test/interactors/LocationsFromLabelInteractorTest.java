df06a61d548cb63ac237fcc72cd15d9909b463a6
04kash <m04kashish@gmail.com>
2023-12-02 00:35:32 -0500

    Added LocationsFromLabelInteractorTest

---
+++ test/interactors/LocationsFromLabelInteractorTest.java
@@ -0,0 +1,50 @@
+package interactors;
+
+import data_access.InMemoryLocationsFromLabel;
+import entity.Coordinate;
+import entity.Location;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import use_case.LocationsFromLabel.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class LocationsFromLabelInteractorTest {
+    LocationsFromLabelUserDataAccessInterface Repository;
+    ArrayList<Location> locations;
+
+    @BeforeEach
+    void unit() throws IOException {
+        locations = new ArrayList<>();
+        locations.add(new Location("The Abbot",new Coordinate(-79.4134750366211,43.70365524291992),"node/7567822674","foods"));
+        Repository = new InMemoryLocationsFromLabel("restaurants",locations);
+    }
+    @Test
+    void successTest() {
+        LocationsFromLabelOutputBoundary successPresenter = new LocationsFromLabelOutputBoundary() {
+            @Override
+            public void prepareSuccessView(LocationsFromLabelOutputData outputData) {
+                Assertions.assertNotNull(outputData.getLocations());
+                assertEquals(locations.get(0).getName(), outputData.getLocations().get(0).getName());
+                assertEquals(locations.get(0).getCoordinate().getLatitude(), outputData.getLocations().get(0).getCoordinate().getLatitude(), 0.01);
+                assertEquals(locations.get(0).getCoordinate().getLongitude(), outputData.getLocations().get(0).getCoordinate().getLongitude(), 0.01);
+                assertEquals(locations.get(0).getOsmLink(), outputData.getLocations().get(0).getOsmLink());
+                assertEquals(locations.get(0).getFilter(), outputData.getLocations().get(0).getFilter());
+
+
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                 Assertions.fail("Use case failure is unexpected.");
+            }
+        };
+        LocationsFromLabelInputData locationsFromLabelInputData = new LocationsFromLabelInputData("restaurants");
+        LocationsFromLabelInteractor locationsFromLabelInteractor = new LocationsFromLabelInteractor(Repository,successPresenter);
+        locationsFromLabelInteractor.execute(locationsFromLabelInputData);
+    }
+}
