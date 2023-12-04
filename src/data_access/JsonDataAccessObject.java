10d283e13f8890050bbffd7cdd91d985a32b9d10
04kash <m04kashish@gmail.com>
2023-12-03 23:20:48 -0500

    Increased test coveage for use_case and entity to 100%

--- src/data_access/JsonDataAccessObject.java
+++ src/data_access/JsonDataAccessObject.java
@@ -10,12 +10,9 @@ import use_case.displayingLabels.DisplayingLabelsUserDataAccessInterface;
 import use_case.login.LoginUserDataAccessInterface;
 import use_case.signup.SignupUserDataAccessInterface;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 
 public class JsonDataAccessObject implements SignupUserDataAccessInterface, LoginUserDataAccessInterface, CreateLabelDataAccessInterface, SavingLocationUserDataAccessInterface, LocationsFromLabelUserDataAccessInterface, DisplayingLabelsUserDataAccessInterface {
